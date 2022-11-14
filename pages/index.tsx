bf91ece77b82fe1f874978b67ea08f722180a56e
Nicholas Varela <nicholasjv@icloud.com>
2022-11-13 21:53:03 -0500

    Fixed load problem using loadState

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Countdown, { calcTimeDelta, formatTimeDelta } from 'react-countdown';
 import logo from '../public/logo2.png'
+import { useEffect, useState } from 'react'
+
 
 
 const renderer = ({ days, hours, minutes, seconds, completed }: any) => {
@@ -18,23 +20,49 @@ const renderer = ({ days, hours, minutes, seconds, completed }: any) => {
 
   }
 };
+
+function ticket() {
+  const [initialRenderComplete, setInitialRenderComplete] = useState(false);
+
+  // This useEffect will only run once, during the first render
+  useEffect(() => {
+    // Updating a state causes a re-render
+    setInitialRenderComplete(true);
+  }, []);
+
+  // initialRenderComplete will be false on the first render and true on all following renders
+  if (!initialRenderComplete) {
+    // Returning null will prevent the component from rendering, so the content will simply be missing from
+    // the server HTML and also wont render during the first client-side render.
+    return null;
+  } else {
+    const date = new Date();
+    return (<Countdown
+      date={new Date('2023-02-01T00:00:00')}
+      renderer={renderer}
+
+      daysInHours={false}
+
+    />)
+  }
+}
 const Home: NextPage = () => {
+  const [initialRenderComplete, setInitialRenderComplete] = useState(false);
+
+  // This useEffect will only run once, during the first render
+
+
   return (
     <div className={styles.countdown}>
       <Head>
-        <title></title>
+        <title>Perfect Match</title>
         <meta name="description" content="Find your Perfect Match" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <h1>miss us....? </h1>
-        <Countdown
-          date={new Date('2023-02-01T00:00:00')}
-          renderer={renderer}
-          daysInHours={false}
-
-        />
-        <Image src={logo} width={100} height= {100}/>
+        <Image src={logo} width={100} height={100} />
+        {ticket()}
       </main>
 
     </div>
