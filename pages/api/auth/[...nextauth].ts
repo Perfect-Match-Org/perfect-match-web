0064b9877dda1e31cbc214f25ea3d51f75cdd3ae
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-01-10 02:39:24 +0530

    added a check

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,12 +14,18 @@ export default NextAuth({
   callbacks: {
     async signIn({ profile }) {
       try {
+        let success = false;
         await connect();
         const user = await getUser(profile);
         if (!user) {
-          createUser(profile);
+          createUser(profile)
+            .then((user) => (success = true))
+            .catch((err) => {
+              console.error(err);
+              success = false;
+            });
         }
-        return true;
+        return success;
       } catch (err) {
         console.error(err);
         return false;
