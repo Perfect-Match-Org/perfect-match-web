2b53547fe0e5a24d62a62246e762cb1c9da12a74
Nicholas Varela <nicholasjv@icloud.com>
2023-01-10 20:14:07 -0600

    Changed <a> tags to <Link>

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,7 +11,7 @@ function Footer() {
         <div>
             <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 light:bg-gray-800" >
 
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">Perfect Match</a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">Perfect Match</Link>. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
