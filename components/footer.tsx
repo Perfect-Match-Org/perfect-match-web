e1cdb38e7ac713a42ae409b93da2bcf81c31a977
Nicholas Varela <nicholasjv@icloud.com>
2023-01-10 20:08:01 -0600

    Fixed apos error with text on about page causing deployment failure

--- components/footer.tsx
+++ components/footer.tsx
@@ -15,7 +15,7 @@ function Footer() {
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
-                        <a href="/" className="mr-4 hover:underline md:mr-6 ">Home</a>
+                        <Link href="/" className="mr-4 hover:underline md:mr-6 ">Home</Link>
                     </li>
                     <li>
                         {/* <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a> */}
