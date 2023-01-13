ced3a963f8f3d71c173808722923c324f05a07c3
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-01-14 04:26:25 +0530

    fix apos err

--- pages/auth/error.tsx
+++ pages/auth/error.tsx
@@ -6,13 +6,13 @@ const Error: NextPage = () => {
     <div className="flex h-screen w-screen justify-center items-center text-red-500">
       <div className="w-1/2">
         <div className="text-xl">
-          Oh no, it looks like we've hit a bit of a roadblock in finding your
-          perfect match.
+          Oh no, it looks like we&apos;ve hit a bit of a roadblock in finding
+          your perfect match.
           <br />
           <br />
           To ensure a successful love connection, please double check that
-          you're using your official Cornell University email address (ending in
-          @cornell.edu) and give it another try.
+          you&apos;re using your official Cornell University email address
+          (ending in @cornell.edu) and give it another try.
           <br />
           <br />
           <GoogleAuth login={true} />
