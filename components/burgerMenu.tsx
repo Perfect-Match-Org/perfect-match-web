ae6372765a3cc36e73387309061f69050e9b762e
Nicholas Varela <nicholasjv@icloud.com>
2023-01-29 17:27:51 -0500

    Hamburger menu with solid background

--- components/burgerMenu.tsx
+++ components/burgerMenu.tsx
@@ -4,31 +4,31 @@ import Link from "next/link";
 
 const Toggle = () => {
     const [show, toggleShow] = React.useState(true);
-    
+
     return (
-      <div>
-        <button className="text-4xl text-gray-500" onClick={() => toggleShow(!show)}>{show ? "☰" : '✕'}</button>    
-            {!show && 
-            <div className="flex">
-                <nav className="z-10 absolute text-center mx-auto top-1/3 w-full right-0">
-                    <ul>
-                        <li className="text-gray-500 text-2xl font-extrabold">
-                            <Link href="/">Home</Link>
-                        </li>
-                        <li className="text-gray-500 text-2xl font-extrabold mt-3">
-                            <Link href="/profile">Profile</Link>
-                        </li>
-                        <li className="text-gray-500 text-2xl font-extrabold mt-3">
-                            <Link href="/statistics">Statistics</Link>
-                        </li>
-                        <li className="text-gray-500 text-2xl font-extrabold mt-3">
-                            <Link href="/about">About & Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>}
-      </div>
+        <div >
+            <button className="text-4xl text-gray-500" onClick={() => toggleShow(!show)}>{show ? "☰" : '✕'}</button>
+            {!show &&
+                <div className="flex">
+                    <nav className="bg-slate-100 z-10 absolute text-center mx-auto  w-full right-0">
+                        <ul>
+                            <li className="text-gray-500 text-2xl font-extrabold">
+                                <Link href="/">Home</Link>
+                            </li>
+                            <li className="text-gray-500 text-2xl font-extrabold mt-3">
+                                <Link href="/profile">Profile</Link>
+                            </li>
+                            <li className="text-gray-500 text-2xl font-extrabold mt-3">
+                                <Link href="/statistics">Statistics</Link>
+                            </li>
+                            <li className="text-gray-500 text-2xl font-extrabold mt-3">
+                                <Link href="/about">About & Contact</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>}
+        </div>
     )
-  }
+}
 
 export default Toggle;
