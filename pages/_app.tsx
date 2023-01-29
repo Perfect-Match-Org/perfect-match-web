d6de3de9d46ee981c1a155f47e7604ec8b735af5
Nicholas Varela <nicholasjv@icloud.com>
2023-01-29 17:56:58 -0500

    Added Vercel Analytics Plugin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   const { session } = pageProps;
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 }
