d7e66d2a7662e3818ee5de6e1c03ec778b30149f
flaviafafa <100969495+flaviafafa@users.noreply.github.com>
2023-01-30 18:00:45 -0500

    Update profileTabs.tsx

--- components/profileTabs.tsx
+++ components/profileTabs.tsx
@@ -10,20 +10,20 @@ function ProfileTabs(props: any) {
   return (
     <div className="w-full items-cente px-2 py-4 sm:px-0">
       <Tab.Group>
-        <Tab.List className="flex space-x-1 px-3 rounded-xl   ">
-          <Tab className="p-2 w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-white-700 hover:bg-gray-400 bg-gray-600 ">
+        <Tab.List className="flex space-x-1 px-0 rounded-xl   ">
+          <Tab className="p-2 w-full rounded-lg py-2.5 text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300 bg-rose-100 ">
             Status
           </Tab>
-          <Tab className="p-2 w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-white-700 hover:bg-gray-400 bg-gray-600 ">
+          <Tab className="p-2 w-full rounded-lg py-2.5 text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300 bg-rose-200 ">
             Profile
           </Tab>
-          <Tab className="w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-white-700 hover:bg-gray-400 bg-gray-600">
+          <Tab className="w-full rounded-lg py-2.5 text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300 bg-rose-300">
             Survey
           </Tab>
-          <Tab className="w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-white-700 hover:bg-gray-400 bg-gray-600 ">
+          <Tab className="w-full rounded-lg py-2.5 text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300 bg-pink-200 ">
             Crushes & Forbidden
           </Tab>
-          <Tab className="w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-white-700 hover:bg-gray-400 bg-gray-600 ">
+          <Tab className="w-full rounded-lg py-2.5 text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300 bg-pink-300 ">
             Matches
           </Tab>
         </Tab.List>
@@ -32,13 +32,13 @@ function ProfileTabs(props: any) {
             <Status optIn={user.optIn} />
           </Tab.Panel>
           <Tab.Panel className="rounded-xl bg-white p-3">
-            <h2 className="w-full py-4 px-10 mr-8 text-3xl text-gray-600 font-extrabold leading-9 md:w-1/3">
+            <h2 className="w-full pt-20 pb-4 mr-8 text-3xl text-gray-500 font-extrabold leading-9 md:w-1/3">
               Profile
             </h2>
             <ProfileComponent />
           </Tab.Panel>
           <Tab.Panel className="rounded-xl bg-white p-3">
-            <h2 className="w-full py-4 px-10 mr-8 text-3xl text-gray-600 font-extrabold leading-9 md:w-1/3">
+            <h2 className="w-full pt-20 pb-4 mr-8 text-3xl text-gray-500 font-extrabold leading-9 md:w-1/3">
               Survey
             </h2>
             <SurveyComponent survey={user.survey} />
