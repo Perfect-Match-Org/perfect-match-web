d6db7e1907b8f1bec1ebb4b03cf46f66049c1bb5
flaviafafa <100969495+flaviafafa@users.noreply.github.com>
2023-01-30 21:04:41 -0500

    Update burgerMenu.tsx

--- components/burgerMenu.tsx
+++ components/burgerMenu.tsx
@@ -9,21 +9,22 @@ const Toggle = () => {
             <button className="text-4xl text-gray-500" onClick={() => toggleShow(!show)}>{show ? "☰" : '✕'}</button>    
                 {!show && 
                 <div className="flex">
-                    <nav className="mt-3.5 z-10 absolute text-center mx-auto pt-44 bg-white w-full h-full right-0">
+                    <nav className="mt-3.5 z-10 absolute text-center mx-auto bg-white w-full right-0">
                         <ul>
-                            <li className="text-gray-500 text-2xl font-medium">
+                            <li className="text-gray-500 text-xl font-medium mt-4">
                                 <Link href="/">Home💖</Link>
                             </li>
-                            <li className="text-gray-500 text-2xl font-medium mt-4">
+                            <li className="text-gray-500 text-xl font-medium mt-5">
                                 <Link href="/profile">Profile🪪</Link>
                             </li>
-                            <li className="text-gray-500 text-2xl font-medium mt-4">
+                            <li className="text-gray-500 text-xl font-medium mt-5">
                                 <Link href="/statistics">Statistics📈</Link>
                             </li>
-                            <li className="text-gray-500 text-2xl font-medium mt-4">
+                            <li className="text-gray-500 text-xl font-medium mt-5 mb-5">
                                 <Link href="/about">About & Contact👨‍💻</Link>
                             </li>
                         </ul>
+                        <hr className="border-2 border-rose-300" />
                     </nav>
                 </div>}
         </div>
