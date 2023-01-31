8690946f01a62657d21d54b5d6728fd665d50b97
flaviafafa <100969495+flaviafafa@users.noreply.github.com>
2023-01-31 15:43:08 -0500

    added content to FAQ

--- pages/about.tsx
+++ pages/about.tsx
@@ -274,6 +274,17 @@ const About: any = (props: any) => {
                     connect when matches are released.
                   </p>
                 </details>
+                <details className="mb-5">
+                  <summary className="font-semibold  bg-white rounded-md py-2 px-4">
+                  What is the Perfect Match team like?
+                  </summary>
+
+                  <p style={{paddingTop: "10px", paddingLeft: "15px"}}>
+                  There are currently seven members in the team. Each of us focuses 
+                    on designing, developing, or marketing; there is no leader, 
+                    and we make decisions together. 
+                  </p>
+                </details>
               </div>
               <div className="w-full lg:w-1/2 px-4">
                 <details className="mb-5">
@@ -310,15 +321,6 @@ const About: any = (props: any) => {
                 </details>
                 <details className="mb-5">
                   <summary className="font-semibold  bg-white rounded-md py-2 px-4 cursor-pointer">
-                    What is the Perfect Match team like?
-                  </summary>
-
-                  <p style={{ paddingTop: "10px", paddingLeft: "15px" }}>
-                    Will add the answer tmr.
-                  </p>
-                </details>
-                <details className="mb-5">
-                  <summary className="font-semibold  bg-white rounded-md py-2 px-4 cursor-pointer">
                     How can I contact the Perfect Match team?
                   </summary>
 
@@ -339,6 +341,16 @@ const About: any = (props: any) => {
                       Reddit
                     </a>
                     !
+                  </p>
+                </details>
+                <details className="mb-5">
+                  <summary className="font-semibold  bg-white rounded-md py-2 px-4">
+                    How to join the Perfect Match team?
+                  </summary>
+
+                  <p style={{paddingTop: "10px", paddingLeft: "15px"}}>
+                    We welcome new members and new ideas! If you are interested in joining the team, please fill out this <a href="https://forms.gle/Jv5th8zbajVF6qN6A" className="underline">form</a> at any time.
+                    We will contact you soon.
                   </p>
                 </details>
               </div>
