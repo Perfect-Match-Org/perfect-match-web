3a570361a279b35a3cca483f1f2fd9e6690d4b6c
flaviafafa <100969495+flaviafafa@users.noreply.github.com>
2023-02-13 15:19:08 -0500

    added heart beating animation

--- components/profile-tabs/matches.tsx
+++ components/profile-tabs/matches.tsx
@@ -19,21 +19,72 @@ function Matches(props: any) {
   })
 
   return (
-    <section className="bg-gray-200">
-      <div className="mx-flex my-flex   lg:items-center">
-        <section className="bg-white ">
-          <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
-            <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Your Matches are Here!</h2>
-              <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">We&apos;ve crunched the numbers, analysed your responses, ran the models, chatted with our in-house love specialists, and consulted Touchdown. Here are your Perfect Matches! </p>
+    <div>
+      <div className={styles.container}>
+          <div className={styles.main}>
+          <button className={styles.absolute4} onClick={() => setVisible(!visible)}>
+            {visible ? '' : '💌'}
+          </button>
+          {!visible && 
+            <div className={styles.absolute}>
+              <div className={styles.layer1}></div>
             </div>
-
-            {list}
-
+          }
+          {visible && 
+            <div className={styles.absolute}>
+              <div className={styles.layer1_fading}></div>
+            </div>
+          }
+          {!visible && 
+            <div className={styles.absolute1}>
+              <div className={styles.layer2}></div>
+            </div>
+          }
+          {visible && 
+            <div className={styles.absolute1}>
+              <div className={styles.layer2_fading}></div>
+            </div>
+          }
+          {!visible && 
+            <div className={styles.absolute2}>
+              <div className={styles.layer3}></div>
+            </div>
+          }
+          {visible && 
+            <div className={styles.absolute2}>
+              <div className={styles.layer3_fading}></div>
+            </div>
+          }
+          {!visible && 
+            <div className={styles.absolute3}>
+              <div className={styles.layer4}></div>
+            </div>
+          }
+          {visible && 
+            <div className={styles.absolute3}>
+              <div className={styles.layer4_fading}></div>
+            </div>
+          }
           </div>
-        </section>
+        </div>
+          {visible &&
+            <section className={styles.match_list}>
+              <div className="mx-flex my-flex   lg:items-center">
+                <section className="bg-white ">
+                  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+                    <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+                      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Your Matches are Here!</h2>
+                      <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">
+                        We&apos;ve crunched the numbers, analysed your responses, ran the models, chatted with our in-house love specialists, and consulted Touchdown. Here are your Perfect Matches!
+                      </p>
+                    </div>
+                    {list}
+                  </div>
+                </section>
+              </div>
+            </section>
+          }
       </div>
-    </section>
   );
 }
 
