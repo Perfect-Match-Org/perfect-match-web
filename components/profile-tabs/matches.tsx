72f776d55b26b7315235d942298bc1f2dced1d60
Nicholas Varela <nicholasjv@icloud.com>
2023-02-13 02:32:52 -0500

    reformated matches page

--- components/profile-tabs/matches.tsx
+++ components/profile-tabs/matches.tsx
@@ -13,12 +13,12 @@ function Matches(props: any) {
 
   return (
     <section className="bg-gray-200">
-      <div className="mx-auto max-w-screen-xl  lg:flex lg:h-screen lg:items-center">
+      <div className="mx-flex my-flex   lg:items-center">
         <section className="bg-white ">
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
             <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
               <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Your Matches are Here!</h2>
-              <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">We&apos;ve crunched the numbers, analysed your responses, ran the models, chatted with our in-house love specialists, and consulted Touchdown. Here are your Perfect Matches. </p>
+              <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">We&apos;ve crunched the numbers, analysed your responses, ran the models, chatted with our in-house love specialists, and consulted Touchdown. Here are your Perfect Matches! </p>
             </div>
 
             {list}
---
+++ pages/privacy.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+import Link from 'next/link'
+import Matches from "../components/profile-tabs/matches";
+
+const Home: any = () => {
+    return (
+        <div><Matches /></div>)
+};
+
+export default Home;
