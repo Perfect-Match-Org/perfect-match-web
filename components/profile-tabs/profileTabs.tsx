1b2ea4e3bb58037444fb7c58fb222e6db403facb
Nicholas Varela <nicholasjv@icloud.com>
2023-02-13 15:13:37 -0500

    Fixed match page

--- components/profile-tabs/profileTabs.tsx
+++ components/profile-tabs/profileTabs.tsx
@@ -19,23 +19,8 @@ function ProfileTabs(props: any) {
   const user = props.user;
   return (
     <div className="w-full items-center px-2 py-4 sm:px-0">
-      <Tab.Group defaultIndex={tabIndex[section]}>
-        <Tab.List className="flex space-x-1 px-0 rounded-xl">
-          <Tab
-            className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300"
-            style={{ background: "#ffebed" }}
-          >
-            Matches
-          </Tab>
+      <Matches matches={user.matches} refresh={props.refresh} />
 
-        </Tab.List>
-        <Tab.Panels className="mt-2">
-
-          <Tab.Panel className="rounded-xl bg-white p-3">
-            <Matches matches={user.matches} refresh={props.refresh} />
-          </Tab.Panel>
-        </Tab.Panels>
-      </Tab.Group>
     </div>
   );
 }
