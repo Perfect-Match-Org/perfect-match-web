5890fefbd9336221e646dc0d1bfbf6beff8906de
Jason Katz <katz.jason37@gmail.com>
2023-02-13 18:23:25 -0500

    hover icon SM

--- components/profile-tabs/match_tile.tsx
+++ components/profile-tabs/match_tile.tsx
@@ -31,15 +31,23 @@ function Match_Tile(props: any) {
                     <p className="mb-3 sm:mb-4 text-gray-500">First song on my hookup playlist...🎶<span className="font-bold"> {match_data.hookupsong}</span></p>
                     <p className="mb-4 sm:mb-6 text-gray-500">Bio...<span className="font-bold">{match_data.bio}</span></p>
 
+
+                    <div data-popover id="popover-default" role="tooltip" className="absolute z-10 invisible inline-block w-64 text-sm font-light text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+                        <div className="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                            <h3 className="font-semibold text-gray-900 dark:text-white">Popover title</h3>
+                        </div>
+                        <div className="px-3 py-2">
+                            <p>And here's some amazing content. It's very engaging. Right?</p>
+                        </div>
+                        <div data-popper-arrow></div>
+                    </div>
+
                     <ul className="flex space-x-4 sm:mt-0">
-                        {match_data.insta != "" && (
-                            <li>
-                                <a href={"https://instagram.com"} className="text-gray-500 hover:text-gray-900 ">
-                                    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
-                                </a>
-                            </li>
-                            //match_data.survey.contact.insta
-                        )};
+                        <li>
+                            <a data-popover-target="popover-default" href={"https://instagram.com"} className="text-gray-500 hover:text-gray-900 ">
+                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                            </a>
+                        </li>
 
                         {match_data.twitter != "NULL" && (
                             <li>
@@ -71,8 +79,8 @@ function Match_Tile(props: any) {
                             </a>
                         </li> */}
                     </ul>
-                </div>
-            </div>
+                </div >
+            </div >
 
 
         </div >
