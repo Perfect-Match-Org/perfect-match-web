8ca339ee472b783bf6ea88c7c904895660a9141a
Nicholas Varela <nicholasjv@icloud.com>
2023-02-13 01:38:49 -0500

    Added profile tabs

--- components/profile-tabs/matches.tsx
+++ components/profile-tabs/matches.tsx
@@ -1,20 +1,30 @@
+import Match_Tile from "./match_tile";
+
 function Matches(props: any) {
-  let randomNum = Math.floor(Math.random() * 10);
-  let nameToReturn = "Romeo";
-  if (randomNum % 2 == 0) {
-    nameToReturn = "Juliet";
-  }
+
+
+  const products = ['orange', 'apple', 'watermelon'];
+
+  const list: any = []
+
+  products.forEach((product) => {
+    list.push(<li><Match_Tile></Match_Tile></li>)
+  })
+
   return (
-   <section className="bg-gray-200">
+    <section className="bg-gray-200">
       <div className="mx-auto max-w-screen-xl  lg:flex lg:h-screen lg:items-center">
-        <div className="mx-auto max-w-xl text-center">
-          <h1 className="text-3xl font-extrabold text-rose-400 sm:text-5xl">
-            Slow down, {nameToReturn}...
-          </h1>
-          <p className="mt-4 sm:text-xl text-gray-500 sm:leading-relaxed">
-            Matches aren&apos;t released yet. Check back on Monday, Feb. 13!
-          </p>
-        </div>
+        <section className="bg-white ">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+            <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Your Matches are Here!</h2>
+              <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">We&apos;ve crunched the numbers, analysed your responses, ran the models, chatted with our in-house love specialists, and consulted Touchdown. Here are your Perfect Matches. </p>
+            </div>
+
+            {list}
+
+          </div>
+        </section>
       </div>
     </section>
   );
