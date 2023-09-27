f91576313ec3bdfc08504ddca71e65842698286b
Pratyush Sudhakar <ps2245@cornell.edu>
2023-09-27 00:59:04 -0400

    moved members data in members.ts

---
+++ components/members/members.ts
@@ -0,0 +1,99 @@
+export const leads = [
+    {
+        Name: 'Grace Wilkey',
+        ClassYear: 2024,
+        Major: 'Hospitality Administration',
+        Img: '/members/grace.jpg',
+        Team: 'Business',
+        Linkedin: 'https://www.linkedin.com/in/grace-wilkey-201a30147',
+    },
+    {
+        Name: 'Pratyush Sudhakar',
+        ClassYear: 2025,
+        Major: 'Computer Science & Math',
+        Img: '/members/pratyush.jpg',
+        Team: 'Engineering',
+        Linkedin: 'https://www.linkedin.com/in/pratyushsudhakar',
+    },
+    {
+        Name: 'Nicholas Varela',
+        ClassYear: 2025,
+        Major: 'Computer Science & Government',
+        Img: '/members/nick.jpg',
+        Team: 'Engineering',
+        Linkedin: 'https://www.linkedin.com/in/nicholas-varela',
+    },
+];
+
+export const team = [
+    {
+        Name: 'Web Development',
+        Description: 'The Web Development team is responsible for building the front-end and back-end of the Perfect Match website.',
+        Leads: [
+            {
+                Name: 'Nicholas Varela',
+                ClassYear: 2025,
+                Major: 'Computer Science & Government',
+                Img: '/members/nick.jpg',
+                Linkedin: 'https://www.linkedin.com/in/nicholas-varela',
+            },
+        ],
+    },
+    {
+        Name: 'Algorithms',
+        Description: 'The Algorithms team is responsible for building the matching algorithm that matches students based on their preferences.',
+        Leads: [
+            {
+                Name: 'Taerim Eom',
+                ClassYear: 2024,
+                Major: 'Computer Science',
+                Img: '/members/taerim.jpg',
+                Linkedin: 'https://www.linkedin.com/in/taerim-eom-301a41197',
+            },
+        ],
+        Members: [
+            {
+                Name: 'Vipin Gunda',
+                ClassYear: 2025,
+                Major: 'Computer Science & Math',
+                Img: '/members/vipin.jpg',
+                Linkedin: 'https://www.linkedin.com/in/vipin-gunda',
+            },
+        ],
+    },
+    {
+        Name: 'Data Analysis',
+        Description: 'The Data Analysis team is responsible for analyzing the data collected from the Perfect Match survey.',
+        Leads: [
+            {
+                Name: 'Flavia Jiang',
+                ClassYear: 2025,
+                Major: 'Information Science',
+                Img: '/members/person.jpg',
+                Linkedin: 'https://www.linkedin.com/in/ziyan-jiang-73b882255',
+            },
+        ],
+    },
+    {
+        Name: 'Business',
+        Description: 'The Business team is responsible for marketing Perfect Match and reaching out to potential sponsors.',
+        Leads: [
+            {
+                Name: 'Grace Wilkey',
+                ClassYear: 2024,
+                Major: 'Hospitality Administration',
+                Img: '/members/grace.jpg',
+                Linkedin: 'https://www.linkedin.com/in/grace-wilkey-201a30147',
+            },
+        ],
+        Members: [
+            {
+                Name: 'Varshini Madhavan',
+                ClassYear: 2025,
+                Major: 'Industrial and Labor Relations',
+                Img: '/members/varshini.jpg',
+                Linkedin: 'https://www.linkedin.com/in/varshinimadhavan',
+            },
+        ],
+    },
+];
