172a0973740119acbab27e540a694ff5546cea35
Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>
2023-10-12 20:56:27 -0400

    Add members to the about section of the website

---
+++ components/members/members.ts
@@ -0,0 +1,73 @@
+export const team = [
+    {
+        Name: 'Engineering',
+        Description: 'There are three subteams of Engineering: Web Development, Algorithms, and Data Analysis. The Web Dev subteam builds the front-end and back-end of the Perfect Match website. The Algorithms subteam improves the matching algorithm that matches students based on their preferences. The Data Analysis subteam analyzes and visualizes the data collected from the Perfect Match survey.',
+        Members: [
+            {
+                Name: 'Nicholas Varela',
+                ClassYear: 2025,
+                Major: 'CS & Public Policy',
+                Img: '/members/nick.jpg',
+                Linkedin: 'https://www.linkedin.com/in/nicholas-varela',
+                Title1: 'Engineering Team Co-Lead',
+                Title2: 'Web Dev Subteam Head'
+            },
+            {
+                Name: 'Pratyush Sudhakar',
+                ClassYear: 2025,
+                Major: 'CS & Math',
+                Img: '/members/pratyush.jpg',
+                Team: 'Engineering',
+                Linkedin: 'https://www.linkedin.com/in/pratyushsudhakar',
+                Title1: 'Engineering Team Co-Lead',
+                Title2: 'Web Dev Member'
+            },
+            {
+                Name: 'Flavia Jiang',
+                ClassYear: 2025,
+                Major: 'Info Sci & Stats',
+                Img: '/members/flavia.jpg',
+                Title1: 'Data Analysis Subteam Head',
+                Title2: 'Web Dev Member'
+            },
+            {
+                Name: 'Taerim Eom',
+                ClassYear: 2024,
+                Major: 'CS',
+                Img: '/members/taerim.jpg',
+                Linkedin: 'https://www.linkedin.com/in/taerim-eom-301a41197',
+                Title1: 'Algorithms Subteam Head',
+            },
+            {
+                Name: 'Vipin Gunda',
+                ClassYear: 2025,
+                Major: 'CS & Math',
+                Img: '/members/vipin.jpg',
+                Linkedin: 'https://www.linkedin.com/in/vipin-gunda',
+                Title1: 'Algorithms Subteam Member',
+            },
+        ],
+    },
+    {
+        Name: 'Business',
+        Description: 'The Business team is responsible for marketing Perfect Match and reaching out to potential sponsors.',
+        Members: [
+            {
+                Name: 'Grace Wilkey',
+                ClassYear: 2024,
+                Major: 'Hospitality Administration',
+                Img: '/members/grace.jpg',
+                Linkedin: 'https://www.linkedin.com/in/grace-wilkey-201a30147',
+                Title1: 'Business Team Lead',
+            },
+            {
+                Name: 'Varshini Madhavan',
+                ClassYear: 2025,
+                Major: 'Industrial and Labor Relations',
+                Img: '/members/varshini.jpg',
+                Linkedin: 'https://www.linkedin.com/in/varshinimadhavan',
+                Title1: 'Business Team Member',
+            },
+        ],
+    },
+];
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import Members from '../components/members';
 
 const About: any = (props: any) => {
     return (
@@ -13,7 +14,7 @@ const About: any = (props: any) => {
             <div>
                 <div>
                     {' '}
-                    <section className="bg-white ">
+                    <section className="bg-white">
                         <div
                             className="gap-6 lg:gap-10 items-center px-0 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:px-6 flex-col lg:flex-row mx-[5%] sm:mx-[10%] lg:mx-[15%] sm:py-16 lg:py-20 py-12"
                             style={{ display: 'flex', paddingLeft: '0px' }}
@@ -24,12 +25,10 @@ const About: any = (props: any) => {
                                 </h2>
 
                                 <p className="sm:mb-4 mb-0">
-                                    {' '}
-                                    Perfect Match is a matchmaking survey ideated in February 2019. Our machine learning
-                                    algorithm uses your survey to pair you with other Cornell students — your Perfect
-                                    Matches! This year, we are back with fun survey questions, improved matching
-                                    algorithm, and better privacy protection measures! Don&apos;t hesitate to get in for
-                                    a Valentine&apos;s Day you won&apos;t forget!
+                                    Perfect Match is a matchmaking survey ideated in February 2019. Our machine learning algorithm uses your survey to pair you
+                                    with other Cornell students — your Perfect Matches! This year, we are back with fun survey questions, improved matching
+                                    algorithm, and better privacy protection measures! Don&apos;t hesitate to get in for a Valentine&apos;s Day you won&apos;t
+                                    forget!
                                 </p>
                                 {/* <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p> */}
                             </div>
@@ -40,16 +39,16 @@ const About: any = (props: any) => {
                     </section>
                 </div>
 
-                <section className="bg-white pt-3 sm:pb-10 pb-5">
+                <section className="bg-pink-100 pt-0 pb-12 sm:pb-10 pb-5">
                     <div className="mx-[10%] lg:mx-[15%]">
                         <ol className="hidden sm:block sm:border-xl-0 sm:border-t-2 border-rose-300 sm:flex sm:gap-6">
                             <li>
                                 <div className="flex sm:block flex-start items-center pt-2 sm:pt-0">
-                                    <div className="sm:-mt-4 text-2xl -ml-1">💓</div>
+                                    <div className="sm:-mt-5 text-3xl -ml-1">💓</div>
                                     <p className="text-rose-400 text-l my-2 font-bold">02-01-23 5PM</p>
                                 </div>
                                 <div className="mt-0.5 ml-4 sm:ml-0 pb-5">
-                                    <h4 className="text-gray-800 font-semibold text-xl mb-1.5">PM2023 is Launched!</h4>
+                                    <h4 className="text-gray-700 font-semibold text-xl mb-1.5">PM2023 is Launched!</h4>
                                     <p className="text-gray-500 mb-3">
                                         Sign in with your Cornell email. Then fill out your profile and the survey! For
                                         updates on PM23, follow us on{' '}
@@ -67,23 +66,21 @@ const About: any = (props: any) => {
                             </li>
                             <li>
                                 <div className="flex sm:block flex-start items-center pt-2 sm:pt-0">
-                                    <div className="sm:-mt-4 text-2xl">💘</div>
+                                    <div className="sm:-mt-5 text-3xl">💘</div>
                                     <p className="text-rose-400 text-l my-2 font-bold">02-13-23 Noon</p>
                                 </div>
                                 <div className="mt-0.5 ml-4 sm:ml-0 pb-5">
-                                    <h4 className="text-gray-800 font-semibold text-xl mb-1.5">PM Closes!</h4>
-                                    <p className="text-gray-500 mb-3">
-                                        Hurry up! Cupid is flying away! Make sure to submit your response on time.
-                                    </p>
+                                    <h4 className="text-gray-700 font-semibold text-xl mb-1.5">PM Closes!</h4>
+                                    <p className="text-gray-500 mb-3">Hurry up! Cupid is flying away! Make sure to submit your response on time.</p>
                                 </div>
                             </li>
                             <li>
                                 <div className="flex sm:block flex-start items-center pt-2 sm:pt-0">
-                                    <div className="sm:-mt-4 text-2xl">💞</div>
+                                    <div className="sm:-mt-5 text-3xl">💞</div>
                                     <p className="text-rose-400 text-l my-2 font-bold">02-13-23 Night</p>
                                 </div>
                                 <div className="mt-0.5 ml-4 sm:ml-0 pb-5">
-                                    <h4 className="text-gray-800 font-semibold text-xl mb-1.5">Matches Out!</h4>
+                                    <h4 className="text-gray-700 font-semibold text-xl mb-1.5">Matches Out!</h4>
                                     <p className="text-gray-500 mb-3">
                                         An email will be sent to you when your perfect matches are out. Go ahead and
                                         shoot your shot!{' '}
@@ -92,16 +89,12 @@ const About: any = (props: any) => {
                             </li>
                             <li>
                                 <div className="flex sm:block flex-start items-center pt-2 sm:pt-0">
-                                    <div className="sm:-mt-4 text-2xl">❤️‍🔥</div>
+                                    <div className="sm:-mt-5 text-3xl">❤️‍🔥</div>
                                     <p className="text-rose-400 text-l my-2 font-bold">02-14-23</p>
                                 </div>
                                 <div className="mt-0.5 ml-4 sm:ml-0 pb-5">
-                                    <h4 className="text-gray-800 font-semibold text-xl mb-1.5">
-                                        It&apos;s Valentine&apos;s Day!
-                                    </h4>
-                                    <p className="text-gray-500 mb-3">
-                                        It&apos;s that time of the year! Grab some food with your perfect matches!
-                                    </p>
+                                    <h4 className="text-gray-700 font-semibold text-xl mb-1.5">It&apos;s Valentine&apos;s Day!</h4>
+                                    <p className="text-gray-500 mb-3">It&apos;s that time of the year! Grab some food with your perfect matches!</p>
                                 </div>
                             </li>
                         </ol>
@@ -113,7 +106,7 @@ const About: any = (props: any) => {
                                     <p className="text-rose-400 text-l font-bold">02-01-23 5PM</p>
                                 </div>
                                 <div className="mt-0.5 ml-4 mb-6">
-                                    <h4 className="text-gray-800 font-semibold text-xl mb-1.5">PM2023 is Launched!</h4>
+                                    <h4 className="text-gray-700 font-semibold text-lg mb-1.5">PM2023 is Launched!</h4>
                                     <p className="text-gray-500 mb-3">
                                         Sign in with your Cornell email. Then fill out your profile and the survey! For
                                         updates on PM23, follow us on{' '}
@@ -135,19 +128,17 @@ const About: any = (props: any) => {
                                     <p className="text-rose-400 text-l font-bold">02-13-23 Noon</p>
                                 </div>
                                 <div className="mt-0.5 ml-4 mb-6">
-                                    <h4 className="text-gray-800 font-semibold text-xl mb-1.5">PM Closes!</h4>
-                                    <p className="text-gray-500 mb-3">
-                                        Hurry up! Cupid is flying away! Make sure to submit your response on time.
-                                    </p>
+                                    <h4 className="text-gray-700 font-semibold text-lg mb-1.5">PM Closes!</h4>
+                                    <p className="text-gray-500 mb-3">Hurry up! Cupid is flying away! Make sure to submit your response on time.</p>
                                 </div>
                             </li>
                             <li>
                                 <div className="flex flex-start items-center pt-2">
                                     <div className="-ml-3 mr-3 text-2xl">💞</div>
-                                    <p className="text-rose-400 text-l font-bold">02-13-23 Night</p>
+                                    <p className="text-rose-400 font-bold">02-13-23 Night</p>
                                 </div>
                                 <div className="mt-0.5 ml-4 pb-5">
-                                    <h4 className="text-gray-800 font-semibold text-xl mb-1.5">Matches Out!</h4>
+                                    <h4 className="text-gray-700 font-semibold text-lg mb-1.5">Matches Out!</h4>
                                     <p className="text-gray-500 mb-3">
                                         An email will be sent to you when your perfect matches are out. Go ahead and
                                         shoot your shot!
@@ -157,19 +148,19 @@ const About: any = (props: any) => {
                             <li>
                                 <div className="flex flex-start items-center pt-2">
                                     <div className="-ml-3 mr-3 text-2xl">❤️‍🔥</div>
-                                    <p className="text-rose-400 text-l font-bold">02-14-23</p>
+                                    <p className="text-rose-400 font-bold">02-14-23</p>
                                 </div>
                                 <div className="mt-0.5 ml-4 pb-5">
-                                    <h4 className="text-gray-800 font-semibold text-xl mb-1.5">
-                                        It&apos;s Valentine&apos;s Day!
-                                    </h4>
-                                    <p className="text-gray-500 mb-3">
-                                        It&apos;s that time of the year! Grab some food with your perfect matches!
-                                    </p>
+                                    <h4 className="text-gray-700 font-semibold text-lg mb-1.5">It&apos;s Valentine&apos;s Day!</h4>
+                                    <p className="text-gray-500 mb-3">It&apos;s that time of the year! Grab some food with your perfect matches!</p>
                                 </div>
                             </li>
                         </ol>
                     </div>
+                </section>
+
+                <section className="bg-white">
+                    <Members />
                 </section>
 
                 <section className="text-gray-500 bg-pink-100">
@@ -187,9 +178,8 @@ const About: any = (props: any) => {
                                     </summary>
 
                                     <p style={{ paddingTop: '10px', paddingLeft: '15px' }}>
-                                        Perfect Match is a matchmaking service for Cornell students and faculty. It uses
-                                        machine learning algorithms to match participants based on their responses to
-                                        the survey. Every year, the survey opens in early Feburary and has thousands of
+                                        Perfect Match is a matchmaking service for Cornell students and alumni. It uses machine learning algorithms to match
+                                        participants based on their responses to the survey. Every year, the survey opens in early Feburary and has thousands of
                                         participants. Matches are released the weekend before Valentine&apos;s day.
                                     </p>
                                 </details>
@@ -218,8 +208,8 @@ const About: any = (props: any) => {
                                     </summary>
 
                                     <p style={{ paddingTop: '10px', paddingLeft: '15px' }}>
-                                        There are currently seven members in the team. Each of us focuses on designing,
-                                        developing, or marketing; there is no leader, and we make decisions together.
+                                        There are currently seven members in the team. It is a small team; every member has their unique strengths and responsibilities.
+                                        We typically meet every month to decide on tasks and assign them. However, every January is still a super busy time!
                                     </p>
                                 </details>
                             </div>
@@ -242,15 +232,15 @@ const About: any = (props: any) => {
                                         What happens to my data?
                                     </summary>
 
-                                    <p style={{ paddingTop: '10px', paddingLeft: '15px' }}>
-                                        Your data is safe with us! We will never share your data with a third party, and
-                                        we will only interact with your information as needed to resolve user issues. We
-                                        may collect anonymous statistics to improve our algorithm, but your identity
-                                        will always be separated from such reports. More FAQ&apos;s about user privacy
-                                        can be found{' '}
-                                        <div className="underline inline">
-                                            <Link href="/statistics#privacy">here</Link>
-                                        </div>
+                                    <p className="pt-2 pl-4">
+                                        Your data is safe with us! We will never share your data with a third party, and we will only interact with your
+                                        information as needed to resolve user issues. We may collect anonymous statistics to improve our algorithm, but your
+                                        identity will always be separated from such reports. More FAQ&apos;s about user privacy can be found{' '}
+                                        <u>
+                                            <Link href="/statistics#privacy" className="underline">
+                                                here
+                                            </Link>
+                                        </u>{' '}
                                         .
                                     </p>
                                 </details>
@@ -307,7 +297,7 @@ const About: any = (props: any) => {
             </div>
             <div>
                 <section className="bg-white ">
-                    <div className="py-8 mx-auto max-w-screen-xl lg:py-16 mx-[5%] sm:mx-[15%]">
+                    <div className="py-8 mx-auto max-w-screen-xl lg:py-16 mx-[5%] sm:mx-[10%] lg:mx-[15%]">
                         <div className="max-w-screen-lg text-gray-500 sm:text-lg ">
                             <h2 className="mb-4 text-2xl sm:text-4xl tracking-tight font-extrabold text-rose-400 ">
                                 Contact Us!
