7ca13e81c801297c9b0580c2e70465450b2d0e2b
Pratyush Sudhakar <ps2245@cornell.edu>
2023-10-26 21:20:47 -0400

    finish feedback feature outlint

--- components/profile-tabs/profileTabs.tsx
+++ components/profile-tabs/profileTabs.tsx
@@ -5,9 +5,20 @@ import ProfileComponent from './profile-section';
 import Crushes from './crushes';
 import Matches from './matches';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { ISurveyFeedback } from '../../database/models';
 
 function ProfileTabs(props: any) {
     const router = useRouter();
+    const user = props.user;
+
+    const [feedback, setFeedback] = useState<ISurveyFeedback>({
+        categoryRanking: [],
+        surveyFeedback: '',
+        valentinesDayImpact: [],
+        otherValentinesDayImpact: '',
+    });
+
     const section = router.asPath.split('#')[1];
     const tabIndex: Record<string, number> = {
         status: 0,
@@ -16,9 +27,89 @@ function ProfileTabs(props: any) {
         crushes: 3,
         matchReviews: 4,
     };
-    const user = props.user;
+
+    const handleFeedbackSubmit = async () => {
+        try {
+            const response = await fetch('/api/profile', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ ...user, feedback }),
+            });
+            if (response.ok) props.refresh();
+            else console.error('Failed to submit feedback');
+        } catch (error) {
+            console.error('There was an error updating the feedback:', error);
+        }
+    };
+
     return (
         <div className="w-full items-center px-2 py-4 sm:px-0">
+            {/* General Feedback Section */}
+            <div className="mt-6">
+                <details className="text-gray-600 border rounded-md p-4">
+                    <summary className="cursor-pointer mb-2">General Feedback (Click to expand)</summary>
+                    {/* Feedback content */}
+                    <div className="space-y-4">
+                        {/* Category Ranking */}
+                        <div>
+                            <label htmlFor="categoryRanking">Rank the category of questions:</label>
+                            <input
+                                type="text"
+                                id="categoryRanking"
+                                value={feedback.categoryRanking.join(',')}
+                                onChange={(e) =>
+                                    setFeedback({ ...feedback, categoryRanking: e.target.value.split(',').map(Number) })
+                                }
+                                className="w-full p-2 mt-2 border rounded-md"
+                                placeholder="E.g.: 1,2,3,4"
+                            />
+                        </div>
+
+                        {/* Survey Feedback */}
+                        <div>
+                            <label htmlFor="surveyFeedback">Feedback about current survey questions:</label>
+                            <textarea
+                                id="surveyFeedback"
+                                rows={4}
+                                value={feedback.surveyFeedback}
+                                onChange={(e) => setFeedback({ ...feedback, surveyFeedback: e.target.value })}
+                                className="w-full p-2 mt-2 border rounded-md"
+                            ></textarea>
+                        </div>
+
+                        {/* Valentine's Day Impact */}
+                        <div>
+                            <label htmlFor="valentinesDayImpact">How did Valentine's Day impact your experience?</label>
+                            <input
+                                type="text"
+                                id="valentinesDayImpact"
+                                value={feedback.valentinesDayImpact.join(',')}
+                                onChange={(e) =>
+                                    setFeedback({ ...feedback, valentinesDayImpact: e.target.value.split(',') })
+                                }
+                                className="w-full p-2 mt-2 border rounded-md"
+                                placeholder="E.g.: 1,2,3,4"
+                            />
+                        </div>
+
+                        {/* Other Valentine's Day Impact */}
+                        <div>
+                            <label htmlFor="otherValentinesDayImpact">Other Valentine's Day Impact:</label>
+                            <textarea
+                                id="otherValentinesDayImpact"
+                                rows={4}
+                                value={feedback.otherValentinesDayImpact}
+                                onChange={(e) => setFeedback({ ...feedback, otherValentinesDayImpact: e.target.value })}
+                                className="w-full p-2 mt-2 border rounded-md"
+                            ></textarea>
+                        </div>
+
+                        <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                            Submit
+                        </button>
+                    </div>
+                </details>
+            </div>
             <Matches matches={user.matchReviews} userId={user._id} refresh={props.refresh} />
         </div>
     );
---
+++ database/models/feedback.ts
@@ -0,0 +1,43 @@
+import { Schema } from 'mongoose';
+
+export interface IMatchFeedback {
+    overallRating: number;
+    topReasonForRating: string;
+    metMatch: boolean;
+    initialRatingDifference: boolean;
+    numberOfDates: number;
+    inRelationshipWithMatch: boolean;
+    additionalComments: string;
+    dateSubmitted?: Date;
+}
+
+export interface ISurveyFeedback {
+    categoryRanking: number[];
+    surveyFeedback: string;
+    valentinesDayImpact: string[];
+    otherValentinesDayImpact: string;
+}
+
+export const matchFeedbackSchema: Schema = new Schema<IMatchFeedback>(
+    {
+        overallRating: { type: Number, required: true },
+        topReasonForRating: { type: String, required: true },
+        metMatch: { type: Boolean, required: true },
+        initialRatingDifference: { type: Boolean, required: true },
+        numberOfDates: { type: Number, required: true },
+        inRelationshipWithMatch: { type: Boolean, required: true },
+        additionalComments: { type: String, required: true },
+        dateSubmitted: { type: Date, required: false },
+    },
+    { _id: false },
+);
+
+export const surveyFeedbackSchema: Schema = new Schema<ISurveyFeedback>(
+    {
+        categoryRanking: [{ type: Number, required: true }],
+        surveyFeedback: { type: String, required: true },
+        valentinesDayImpact: [{ type: String, required: true }],
+        otherValentinesDayImpact: { type: String, required: false },
+    },
+    { _id: false },
+);
