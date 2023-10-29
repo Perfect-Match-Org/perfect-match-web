eba565cbe2753511b10df469e03a096699868bdb
Nicholas Varela <nicholasjv@icloud.com>
2023-10-28 23:13:39 -0400

    Changed Wording, Added Steps

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -27,17 +27,24 @@ const Profile: NextPage = (props: any) => {
                         <div className="content-center flex">
                             <p className="text-lg mt-8 mx-auto sm:text-xl text-center"><strong className="text-rose-400">NEW:</strong> Fill out the feedback survey below to receive an extra match in 2024❣️</p>
                         </div>
-                        <p className="mt-6 text-center">We really appreciate your feedback and comments!
+                        <p className="mt-6 text-center">We truly appreciate your feedback and comments!
                             They will be used to improve next year&#39;s survey and matching algorithms. Please
-                            kindly complete two parts of the feedback survey: <strong className="text-rose-400">General Feedback and Match-Specific Feedback</strong>. It
-                            will take 5~10 minutes in total.
-                            The General Feedback section is right below. For Match-Specific Feedback, scroll down to
+                            complete the two parts of the feedback survey: <strong className="text-rose-400">General Feedback and Match-Specific Feedback</strong>. It
+                            will take 5~10 minutes in total. We truly appreciate your feedback and comments!
+                        </p>
+                        <p className="mt-6 text-center"> <strong className="text-rose-400">Step 1</strong>
+
+                            The General Feedback section is right below.
+                        </p>
+                        <p className="mt-6 text-center"> <strong className="text-rose-400">Step 1</strong>
+                            <strong className="text-rose-400">Step 2</strong>
+                            For Match-Specific Feedback, scroll down to
                             each of your matches and click &#34;Leave Feedback.&#34;
                         </p>
                         <p className="mt-2 text-center">Again, thanks for your feedback and see you in Febraury❤️‍🔥!</p>
                     </div>
                 </section>
-            </div>
+            </div >
             <div>
                 <section>
                     <div>
@@ -48,7 +55,7 @@ const Profile: NextPage = (props: any) => {
                 </section>
             </div>
             <Footer />
-        </div>
+        </div >
     );
 };
 
