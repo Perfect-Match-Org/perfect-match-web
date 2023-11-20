f1e2cf57cdcc758cc219ab2b05612bd7c3f12d8a
Nicholas Varela <nicholasjv@icloud.com>
2023-11-19 21:43:07 -0500

    Made 2023 Stats Component

---
+++ components/analytics/2022Analytics.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+const Stats2023 = () => {
+    const [show, toggleShow] = React.useState(true);
+
+    return (<div>
+
+
+    </div>)
+};
+
+
+export default Stats2023;
