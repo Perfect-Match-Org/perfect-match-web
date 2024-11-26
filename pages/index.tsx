1ca3f60fb06cc9b5ba8d022a999c13ccb054dc6d
Nicholas Varela <nicholasjv@icloud.com>
2024-11-26 15:26:27 -0600

    removed apos, draft countdown for biz to review

--- pages/index.tsx
+++ pages/index.tsx
@@ -122,7 +122,7 @@ const Home: NextPage = (props: any) => {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                                 </svg>
                                 <p className="text-gray-700">
-                                    Matches are revealed by Valentine's Day and participants are encouraged to reach out to their matches.
+                                    Matches are revealed by Valentine Day and participants are encouraged to reach out to their matches.
                                 </p>
                             </div>
                         </div>
