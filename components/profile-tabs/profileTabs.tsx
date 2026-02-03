442a4117ef28cf8fc5a91b799d6306276114fde9
Ashlie Zhang <ashliezhang@Mac-446.lan>
2026-02-02 19:15:01 -0500

    last minute survey changes

--- components/profile-tabs/profileTabs.tsx
+++ components/profile-tabs/profileTabs.tsx
@@ -323,25 +323,25 @@ function ProfileTabs(props: any) {
             <Tab.Group>
                 <Tab.List className="flex space-x-1 px-0 rounded-xl">
                     <Tab
-                        className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300"
+                        className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-bold leading-5 text-pmred-500 hover:bg-gray-300"
                         style={{ background: '#ffebed' }}
                     >
                         Status
                     </Tab>
                     <Tab
-                        className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300"
+                        className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-bold leading-5 text-pmred-500 hover:bg-gray-300"
                         style={{ background: '#ffd8db' }}
                     >
                         Profile
                     </Tab>
                     <Tab
-                        className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300"
+                        className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-bold leading-5 text-pmred-500 hover:bg-gray-300"
                         style={{ background: '#ffc4c9' }}
                     >
                         Survey
                     </Tab>
                     <Tab
-                        className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-medium leading-5 text-gray-500 hover:bg-gray-300"
+                        className="text-sm p-2 w-full rounded-lg py-2.5 sm:text-lg font-bold leading-5 text-pmred-500 hover:bg-gray-300"
                         style={{ background: '#ffb1b7' }}
                     >
                         Crushes & Forbidden
@@ -357,19 +357,19 @@ function ProfileTabs(props: any) {
                         />
                     </Tab.Panel>
                     <Tab.Panel className="rounded-xl bg-white p-3">
-                        <h2 className="pt-10 w-full sm:pt-20 pb-4 mr-8 text-3xl text-gray-500 font-extrabold leading-9 md:w-1/3">
+                        <h2 className="pt-10 w-full sm:pt-20 pb-4 mr-8 text-3xl text-pmred-500 font-extrabold leading-9 md:w-1/3">
                             Profile
                         </h2>
                         <ProfileComponent profile={user.profile} refresh={props.refresh} />
                     </Tab.Panel>
                     <Tab.Panel className="rounded-xl bg-white p-3">
-                        <h2 className="pt-10 w-full sm:pt-20 pb-4 mr-8 text-3xl text-gray-500 font-extrabold leading-9 md:w-1/3">
+                        <h2 className="pt-10 w-full sm:pt-20 pb-4 mr-8 text-3xl text-pmred-500 font-extrabold leading-9 md:w-1/3">
                             Survey
                         </h2>
                         <SurveyComponent survey={user.survey} refresh={props.refresh} />
                     </Tab.Panel>
                     <Tab.Panel className="rounded-xl bg-white p-3">
-                        <h2 className="pt-10 w-full sm:pt-20 pb-4 text-3xl text-gray-500 font-extrabold leading-9 md:w-1/2">
+                        <h2 className="pt-10 w-full sm:pt-20 pb-4 text-3xl text-pmred-500 font-extrabold leading-9 md:w-1/2">
                             Crushes & Forbidden
                         </h2>
                         <Crushes crushes={user.crushes} forbidden={user.forbidden} refresh={props.refresh} />
