52ecfbbacc1d00d71ba58287b92af3ea77ad72fd
Huajie Zhong <hz642@cornell.edu>
2026-02-12 18:56:10 -0500

    fix contact

--- components/profile-tabs/matches.tsx
+++ components/profile-tabs/matches.tsx
@@ -17,7 +17,7 @@ function Matches({ matches, userId, refresh, matchCount }: any) {
                         key={match._id}
                         matchID={match._id}
                         matchData={matchData}
-                        contact={matchData.survey}
+                        contact={matchData.survey.contact}
                         matchFeedback={matchFeedback}
                         refresh={refresh}
                         mutualCrush={mutualCrush}
