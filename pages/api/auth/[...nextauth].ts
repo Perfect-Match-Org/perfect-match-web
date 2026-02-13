317a550e464cbb42e838485512285eb78ec14fab
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2026-02-13 00:33:01 -0500

    disable checks for google auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -24,8 +24,7 @@ export default NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            
-            checks: ['state', 'nonce'],
+            checks: ['none'],
         }),
     ],
     pages: { error: '/auth/error' },
