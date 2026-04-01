4c287aefa9e470c7971b0ea40df9fd6f97f4b67f
Huajie Zhong <hz642@cornell.edu>
2026-03-31 22:15:02 -0400

    link the player as well :)

--- components/CuffedOrNotImage.tsx
+++ components/CuffedOrNotImage.tsx
@@ -3,16 +3,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 const CuffedOrNotImage: React.FC = () => {
     return (
-        <div className='relative z-10 max-w-[85vw] w-full min-w-fit h-full flex items-center justify-center'>
-            <Image
-                src="/CuffedOrNot.svg"
-                alt="Cuffed or Not"
-                width={500}
-                height={500}
-                priority={true}
-                draggable='false'
-                className="object-contain"
-            />
+        <div className='relative z-10 max-w-[70vw] md:max-w-[85vw] w-full min-w-fit h-full flex items-center justify-center px-4 md:px-0'>
+            <Link href="https://cuffedornot.perfectmatch.ai">
+                <a>
+                    <Image
+                        src="/CuffedOrNot.svg"
+                        alt="Cuffed or Not"
+                        width={500}
+                        height={500}
+                        priority={true}
+                        draggable='false'
+                        className="object-contain cursor-pointer"
+                    />
+                </a>
+            </Link>
             {/* Overlay positioned relative to the parent */}
             <div className='absolute top-[25%] right-[4%] z-20'>
                 <Link href="https://cuffedornot.perfectmatch.ai">
